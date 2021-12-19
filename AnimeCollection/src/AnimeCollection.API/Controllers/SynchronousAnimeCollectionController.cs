d492bf557c2930c6246ce7b1ef4f3f075ff3e92e
Yauheniya Sirotkina <zsirotkina@gmail.com>
2021-12-20 00:16:49 +0300

    add synchronous controller

---
+++ AnimeCollection/src/AnimeCollection.API/Controllers/SynchronousAnimeCollectionController.cs
@@ -0,0 +1,24 @@
+﻿using AnimeCollection.DAL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnimeCollection.API.Controllers
+{
+    [Route("api/synchronousanimecollection")]
+    [ApiController]
+    public class SynchronousAnimeCollectionController : ControllerBase
+    {
+        private readonly IAnimeCollectionRepository _animeCollectionRepository;
+
+        public SynchronousAnimeCollectionController(IAnimeCollectionRepository animeCollectionRepository)
+        {
+            _animeCollectionRepository = animeCollectionRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAnimeCollection()
+        {
+            var animeCollection = _animeCollectionRepository.GetAnimeCollection();
+            return Ok(animeCollection);
+        }
+    }
+}
